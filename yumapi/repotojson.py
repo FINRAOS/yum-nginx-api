95a1e9d76b7d3af9b73c2ecd9d1f35466c23bd60
Tim Ski <marshyski@gmail.com>
2014-11-20 13:50:54 -0500

    Added main contributor pypingou

--- yumapi/repotojson.py
+++ yumapi/repotojson.py
@@ -9,6 +9,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+#  Main contributor is Pierre-Yves Chibon https://github.com/pypingou
 
 import pkg_resources
 import contextlib
